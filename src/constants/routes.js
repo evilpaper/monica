b435fa7c4a3fdb6d0d65f74a7808457a87714d99
evilpaper <pellburt@gmail.com>
2020-07-22 09:11:25 +0200

    Add basic navigation using react router

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const TODOS = "/";
+export const HOWTOS = "/howtos";
+export const TEAM = "/team";
+export const SETTINGS = "/settings";
+export const SIGN_UP = "/signup";
+export const SIGN_IN = "/signin";
