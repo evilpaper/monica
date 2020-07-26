e7e78657e2658979a638a82fd6b5b38dffb77f14
evilpaper <pellburt@gmail.com>
2020-07-26 15:06:01 +0200

    Add signin page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -2,10 +2,27 @@ import React from "react";
 
 export default function SignIn() {
   return (
-    <form className="sign-in">
-      <input name="username" type="email" placeholder="Your email"></input>
-      <input name="password" type="password" placeholder="Passoword"></input>
-      <button>Create account</button>
-    </form>
+    <section className="sign-up-page">
+      <form className="sign-up-form stack">
+        <h1 className="sign-up-header">Welcome back</h1>
+        <div className="small-stack">
+          <label for="email">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="you@example.com"
+          ></input>
+        </div>
+        <div className="small-stack">
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter 8 characters or more"
+          ></input>
+        </div>
+        <button>Create account</button>
+      </form>
+    </section>
   );
 }
