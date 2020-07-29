9ebac5cef554a3c227157a007274f71d30b7aa05
evilpaper <pellburt@gmail.com>
2020-07-29 13:16:02 +0200

    Add content to landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Landing from "./components/Landing";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
-      <Navigation />
+      {!isLoggedIn && <Landing />}
+      {isLoggedIn && <Navigation />}
     </>
   );
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import logo from "../../assets/monica-logo-turquoise.svg";
 
 export default function Landing() {
   return (
-    <section>
-      <h1>Landing.</h1>
-      <p>Here you will find a nice, selling landing page.</p>
+    <section className="landing-section">
+      <img className="landing-logo" src={logo} alt="monica logo"></img>
+      <h1 className="landing-header">Easiest Food Safety for Caters</h1>
+      <p className="landing-copy">
+        Everything your cater business needs to comply to food safety
+        regulations. Free 14 days trial, no credit card needed. You are minutes
+        away too be compliant.
+      </p>
+      <button className="landing-cta">SHOW ME NOW</button>
     </section>
   );
 }
