4197ce6987548c6db1ab042ff07470d1d330d142
evilpaper <pellburt@gmail.com>
2020-08-10 23:19:15 +0200

    Remove progress banner, change back to light theme

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FiCalendar } from "react-icons/fi";
 
 export default function Todos() {
   return (
@@ -7,29 +6,24 @@ export default function Todos() {
       <section className="top-bar">
         {" "}
         <h3 className="unit-name">Greasy Spoon</h3>
-        <button className="to-do-calendar-button">
-          <FiCalendar className="to-do-calendar-button__icon" />
-          View calendar
-        </button>
+        <button className="to-do-calendar-button">View calendar</button>
       </section>
       <div className="to-do-header">
         <h1 className="to-do-heading">Today</h1>
-        <div>
+
+        {/* <div>
           <div className="to-do-todays-status">
-            {/* <FiActivity className="to-do-todays-status__icon" /> */}
             <span className="to-do-todays-status__text">
               60% · Working on it
             </span>
           </div>
           <div className="to-do-todays-status__progressbar"></div>
-        </div>
+        </div> */}
       </div>
       <section className="to-do-checklist">
         <h2 className="to-do-checklist-name">Daily Checks</h2>
         <h4 className="to-do-checklist-status">
-          <span className="to-do-checklist-status__completion">
-            5 min to complete
-          </span>
+          <span className="to-do-checklist-status__completion">5 min</span>
           <span className="to-do-checklist-status__est-time"></span>
         </h4>
         <div className="to-do-checklist-users">
