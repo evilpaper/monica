295bcb54ab9851ceddb020f6a9d0948f6f4e333b
evilpaper <pellburt@gmail.com>
2020-08-17 18:01:16 +0200

    Add upcoming section, fix typos

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -22,7 +22,49 @@ export default function Todos() {
             <h4 className="to-do-checklist-status">
               <span className="to-do-checklist-status__completion">40%</span>
               <span className="to-do-checklist-status__est-time">
-                5 min to complete
+                5 min work
+              </span>
+            </h4>
+            <div className="progress_container">
+              <div className="progress-bar"></div>
+            </div>
+            <h4 className="to-do-checklist-responsible">
+              <span className="to-do-checklist-responsible__text">
+                Responsible: everyone at work
+              </span>
+              <span className="to-do-checklist-status__est-time"></span>
+            </h4>
+            {/* <div className="to-do-checklist-users">
+          <span class="dot dot1">
+            <span class="initials">PL</span>
+          </span>
+          <span class="dot dot2">
+            <span class="initials">MS</span>
+          </span>
+          <span class="dot dot3">
+            <span class="initials">SH</span>
+          </span>
+        </div> */}
+            <div className="to-do-checklist-button-container">
+              <button className="to-do-checklist-list-button__assign">
+                Remind
+              </button>
+              <button className="to-do-checklist-list-button">
+                Go to tasks
+              </button>
+            </div>
+          </section>
+        </section>
+        <section className="to-do-upcoming-section">
+          <div className="to-do-header">
+            <h1 className="to-do-heading">Upcoming</h1>
+          </div>
+          <section className="to-do-checklist">
+            <h2 className="to-do-checklist-name">Weekly Checks</h2>
+            <h4 className="to-do-checklist-status">
+              <span className="to-do-checklist-status__completion">40%</span>
+              <span className="to-do-checklist-status__est-time">
+                10 min work, due tomorrow
               </span>
             </h4>
             <div className="progress_container">
@@ -46,19 +88,14 @@ export default function Todos() {
           </span>
         </div> */}
             <div className="to-do-checklist-button-container">
-              <button className="to-do-chacklist-list-button__assign">
+              <button className="to-do-checklist-list-button__assign">
                 Remind
               </button>
-              <button className="to-do-chacklist-list-button">
+              <button className="to-do-checklist-list-button to-do-checklist-list-button__upcoming">
                 Go to tasks
               </button>
             </div>
           </section>
-        </section>
-        <section className="to-do-upcoming-section">
-          <div className="to-do-header">
-            <h1 className="to-do-heading">Upcoming</h1>
-          </div>
         </section>
       </main>
     </article>
