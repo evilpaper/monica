0b3f958e31b2aa27c65c268ee731e1153364283f
evilpaper <pellburt@gmail.com>
2020-11-05 07:40:22 +0100

    Expand file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
-import Landing from "./components/Landing";
+import Landing from "../src/pages/Landing";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
