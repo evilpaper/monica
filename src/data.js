51e45f25222a295ebe2943c3ccf754616d253319
evilpaper <pellburt@gmail.com>
2020-11-19 20:13:39 +0100

    Complete first checklist

--- src/data.js
+++ src/data.js
@@ -15,6 +15,27 @@ const CHECKLISTS = [
       {
         name: "Other equipment (e.g. oven) is working properly."
       },
+      {
+        name: "Staff are fit for work and wearing clean work clothes."
+      },
+      {
+        name: "Food preparation areas are clean and disinfected (work surfaces, equipment, utensils, etc.)."
+      },
+      {
+        name: "All areas are free from evidence of pest activity."
+      },
+      {
+        name: "There are plenty of handwashing and cleaning materials (soap, paper towels, sanitiser, etc.)."
+      },
+      {
+        name: "Hot running water is available at all sinks and hand wash basins."
+      },
+      {
+        name: "Probe thermometer is working and probe wipes are available."
+      },
+      {
+        name: "Allergen information is accurate for all items on sale."
+      }
     ]
   }
 ]
