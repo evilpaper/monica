a816c101732ee38b7ba7afbf10b428c46364daa0
evilpaper <pellburt@gmail.com>
2020-12-19 16:11:23 +0100

    Update logo and landing page positioning

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -11,14 +11,16 @@ export default function Landing(props) {
     <article className="landing-section">
       <div className="landing-frame">
         <img className="landing-logo" src={logo} alt="monica logo"></img>
-        <h1 className="landing-header">
-          The <span>easy</span> way to <br/>
-          pass food safety <br/>
-          inspections <span> every </span> time 
-        </h1>
-        <p className="landing-copy">
-          A complete food safety managment system for small and medium food businesses.   
-        </p>
+        <div>
+          <h1 className="landing-header">
+            The <span>easy</span> way to <br/>
+            pass food safety <br/>
+            inspections <span> every </span> time 
+          </h1>
+          <p className="landing-copy">
+            A complete food safety managment system for small and medium food businesses.   
+          </p>
+        </div>
         <button onClick={onButtonClick} className="landing-cta">
           TRY FOR FREE NOW
         </button>
