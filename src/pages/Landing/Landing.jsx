5c94d1e75a2210be69337273931c329ed800950d
evilpaper <pellburt@gmail.com>
2021-02-06 21:01:29 +0100

    Add social proof to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../../assets/monica-logo-turquoise.svg";
+import avatar from "../../assets/social-proof-avatar.jpg"
 import "./styles.css";
 
 export default function Landing(props) {
   function onButtonClick() {
     props.handleLoginClick();
   }
+
+  console.log(avatar);
 
   return (
     <article className="landing-section">
@@ -17,8 +20,8 @@ export default function Landing(props) {
             inspections <span> every </span> time 
           </h1>
           <p className="landing-copy">
-            A complete food safety managment system for small and medium food businesses. 
-            All checklists and records your business need.  
+            A complete food safety management app for small and medium food businesses.
+            You only need to invest <strong>5 mins</strong> a day.
           </p>
         </div>
         <button onClick={onButtonClick} className="landing-cta">
@@ -29,6 +32,13 @@ export default function Landing(props) {
           <li className="landing-checkmark"> ✔ No credit card required </li>
           <li className="landing-checkmark"> ✔ Cancel anytime </li>
         </ul>
+        <section className="social-proof">
+          <img src={avatar} className="social-proof-avatar" alt="social-proof-avatar"/>
+          <div className="social-proof-testimonial">
+            <blockquote className="social-proof-testimonial-quote">"Smidigt o enkelt! Sparar massor av tid!"</blockquote>
+            <p className="social-proof-testimonial-name">Frank Lowe · Owner · Greasy Spoon</p>
+          </div>
+        </section>
       </div>
     </article>
   );
